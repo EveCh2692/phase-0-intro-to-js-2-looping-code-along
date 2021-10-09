17a25a64af27472634d99188725171639a35b57a
Evelyn <evelyn.chavez92@yahoo.com>
2021-10-09 12:29:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const names = [Lisa, Kaitlin, Jan]
-const writeCards = function (names){
- for (let i = 0; i < names.length; i++ ){
-     console.log(`Thank you, ${names}, for the wonderful surprise gift!`)
- }
- return names
+const writeCards= function (names, event) {
+    let thankYouCards =[]
+    for ( let i =0; i < names.length; i++){
+        thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
 }
-writeCards(gifts)
+
+const countDown = function (startingNumbers) {
+    while (startingNumbers > 0) {
+        console.log (startingNumbers)
+        startingNumbers -= 1
+    }
+    console.log(startingNumbers)
+}
